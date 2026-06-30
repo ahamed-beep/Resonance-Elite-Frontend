deac29045ad6740c3248dfbce55a664466eb0934
Abdul Ahad <muhamedahamed251@gmail.com>
2026-06-30 15:58:07 +0500

    deployment ready

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
